baa222e9350a687a1504a32fc87b9e6f87e0d840
TrqR <tristanqr@gmail.com>
2025-06-25 23:25:04 +0200

    code promo

--- src/main/java/com/shooza/demo/controllers/CodePromoController.java
+++ src/main/java/com/shooza/demo/controllers/CodePromoController.java
@@ -3,7 +3,7 @@ package com.shooza.demo.controllers;
 import com.shooza.demo.models.CodePromo;
 import com.shooza.demo.repositories.PromoCodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/shooza/demo/controllers/FavoriteController.java
+++ src/main/java/com/shooza/demo/controllers/FavoriteController.java
@@ -1,11 +1,8 @@
 package com.shooza.demo.controllers;
 
 import com.shooza.demo.DTO.FavoriteDTO;
-import com.shooza.demo.DTO.UserDTO;
-import com.shooza.demo.models.Favorite;
 import com.shooza.demo.models.Product;
 import com.shooza.demo.services.FavoriteService;
-import com.shooza.demo.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
